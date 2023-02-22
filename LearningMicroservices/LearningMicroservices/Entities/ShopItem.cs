4e17702e7d60c603a46007d9f00329cbef4bc9af
Paul Pestov <pavel.pestov@gmail.com>
2023-02-22 05:49:55 +0400

    Added db entitiy

---
+++ LearningMicroservices/LearningMicroservices/Entities/ShopItem.cs
@@ -0,0 +1,10 @@
+﻿namespace LM.Shop.Service.Entities
+{
+    public class ShopItem
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
